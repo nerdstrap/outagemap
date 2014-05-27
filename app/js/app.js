76b6c3f7f1d65140b36013b3035fa6b328eeb64c
nerdstrap <geekstrap@live.com>
2014-05-27 15:34:04 -0400

    updates to click handlers

--- app/js/app.js
+++ app/js/app.js
@@ -23,32 +23,36 @@ define(function (require) {
     var outageReportData = require('data/OutageReport');
     var outageMapLegendData = require('data/OutageMapLegend');
 
-    var bodyElements = document.getElementsByTagName('body');
-    if (bodyElements && bodyElements.length > 0) {
-        bodyElements[0].innerHTML = shellTemplate(shellData);
-    }
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
 
-    var shellViewElements = document.getElementsByClassName('shell-view');
-    if (shellViewElements && shellViewElements.length > 0) {
+    var shellViewElement = document.getElementById('shell-view');
+    if (shellViewElement) {
         console.debug('ShellView.render()');
+        shellViewElement.innerHTML = shellTemplate(shellData);
     }
 
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
     var headerViewElement = document.getElementById('header-view');
     if (headerViewElement) {
         console.debug('HeaderView.render()');
         headerViewElement.innerHTML = headerTemplate(headerData);
     }
 
+    var showOutageMapViewButton = document.getElementById('show-outage-map-view-button');
+    var showOutageReportViewButton = document.getElementById('show-outage-report-view-button');
+
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+
     var contentViewElement = document.getElementById('content-view');
     if (contentViewElement) {
         console.debug('ContentView.render()');
         contentViewElement.innerHTML = contentTemplate(contentData);
-    }
-
-    var footerViewElement = document.getElementById('footer-view');
-    if (footerViewElement) {
-        console.debug('FooterView.render()');
-        footerViewElement.innerHTML = footerTemplate(footerData);
     }
 
     var outageMapElement = document.getElementById('outage-map-view');
@@ -63,36 +67,68 @@ define(function (require) {
         outageReportElement.innerHTML = outageReportTemplate(outageReportData);
     }
 
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+
+    var footerViewElement = document.getElementById('footer-view');
+    if (footerViewElement) {
+        console.debug('FooterView.render()');
+        footerViewElement.innerHTML = footerTemplate(footerData);
+    }
+
     var outageMapLegendElement = document.getElementById('outage-map-legend-view');
     if (outageMapLegendElement) {
         console.debug('OutageMapLegendView.render()');
         outageMapLegendElement.innerHTML = outageMapLegendTemplate(outageMapLegendData);
     }
 
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------
+
     var showOutageMapView = function (event) {
         console.debug('showOutageMapView()');
         if (event) {
             event.preventDefault();
         }
-        outageReportElement.classList.add('hidden');
-        outageMapElement.classList.remove('hidden');
+        if (showOutageMapViewButton) {
+            showOutageMapViewButton.classList.add('hidden');
+        }
+        if (showOutageReportViewButton) {
+            showOutageReportViewButton.classList.remove('hidden');
+        }
+        if (outageMapElement) {
+            outageMapElement.classList.remove('hidden');
+        }
+        if (outageReportElement) {
+            outageReportElement.classList.add('hidden');
+        }
     };
+
+    if (showOutageMapViewButton) {
+        showOutageMapViewButton.onclick = showOutageMapView;
+    }
 
     var showOutageReportView = function (event) {
         console.debug('showOutageReportView()');
         if (event) {
             event.preventDefault();
         }
-        outageMapElement.classList.add('hidden');
-        outageReportElement.classList.remove('hidden');
+        if (showOutageMapViewButton) {
+            showOutageMapViewButton.classList.remove('hidden');
+        }
+        if (showOutageReportViewButton) {
+            showOutageReportViewButton.classList.add('hidden');
+        }
+        if (outageMapElement) {
+            outageMapElement.classList.add('hidden');
+        }
+        if (outageReportElement) {
+            outageReportElement.classList.remove('hidden');
+        }
     };
 
-    var showOutageMapViewButton = document.getElementById('show-outage-map-view-button');
-    if (showOutageMapViewButton) {
-        showOutageMapViewButton.onclick = showOutageMapView;
-    }
-
-    var showOutageReportViewButton = document.getElementById('show-outage-report-view-button');
     if (showOutageReportViewButton) {
         showOutageReportViewButton.onclick = showOutageReportView;
     }
