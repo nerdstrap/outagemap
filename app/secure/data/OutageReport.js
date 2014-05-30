244f0c64c93d25ba8dec21b778b228c4a21e0201
nerdstrap <geekstrap@live.com>
2014-05-30 11:22:33 -0400

    cleaned up styles

--- app/secure/data/OutageReport.js
+++ app/secure/data/OutageReport.js
@@ -13,40 +13,40 @@
                 "countyName": "Sullivan Co., TN",
                 "state": "TN",
                 "operatingUnit": "AEP-AP",
-                "repairIssues": 0,
-                "customersWithoutPower": 0,
-                "totalCustomers": 0,
-                "percentage": 0
+                "repairIssues": 3,
+                "customersWithoutPower": 300,
+                "totalCustomers": 500,
+                "percentage": 60
             },
             {
                 "countyName": "Boone Co., WV",
                 "state": "WV",
                 "operatingUnit": "AEP-AP",
-                "repairIssues": 0,
-                "customersWithoutPower": 0,
-                "totalCustomers": 0,
-                "percentage": 0
+                "repairIssues": 1,
+                "customersWithoutPower": 9000,
+                "totalCustomers": 10000,
+                "percentage": 90
             }
         ],
         "states": [
             {
                 "stateName": "TN",
-                "customersWithoutPower": 0,
-                "totalCustomers": 0,
-                "percentage": 0
+                "customersWithoutPower": 500000,
+                "totalCustomers": 1500000,
+                "percentage": 33
             },
             {
                 "id": 1,
                 "stateName": "WV",
-                "customersWithoutPower": 0,
-                "totalCustomers": 0,
-                "percentage": 0
+                "customersWithoutPower": 12,
+                "totalCustomers": 12,
+                "percentage": 100
             }
         ],
         "grandTotalTitleText": "Grand Total&#58;",
-        "customersWithoutPower": 0,
-        "totalCustomers": 0,
-        "percentage": 0,
+        "customersWithoutPower": 10000000,
+        "totalCustomers": 12000000,
+        "percentage": 94,
     };
 
     return outageReportData;
