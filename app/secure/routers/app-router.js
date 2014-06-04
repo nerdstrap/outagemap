a35add6fec1f4234d6fb365ec059637c79da883e
nerdstrap <geekstrap@live.com>
2014-06-04 15:50:25 -0400

    added jquery and backbone

---
+++ app/secure/routers/app-router.js
@@ -0,0 +1,30 @@
+﻿define(function (require) {
+    'use strict';
+
+    var $ = require('jquery'),
+        _ = require('underscore'),
+        Backbone = require('backbone'),
+        ShellView = require('views/ShellView'),
+        oms2aepwebData = require('data/oms2aepweb');
+
+    var AppRouter = Backbone.Router.extend({
+        initialize: function (options) {
+            console.debug('appRouter.initialize');
+            options || (options = {});
+            var currentContext = this;
+            var shellView = new ShellView({
+                el: $('shell-view'),
+                model: oms2aepwebData
+            });
+            shellView.render();
+        },
+        routes: {
+            '': 'outageMap'
+        },
+        outageMap: function () {
+            console.debug('appRouter.outageMap');
+        }
+    });
+
+    return new AppRouter();
+});
