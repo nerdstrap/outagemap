ae65877c30d7b49681a90e683df8cdf380cdea00
kimshuwinter <sshu@aep.com>
2014-06-17 13:43:42 -0400

    Attach events from region name in URL

--- app/secure/views/OutageMapView.js
+++ app/secure/views/OutageMapView.js
@@ -6,7 +6,8 @@
         Backbone = require('backbone'),
         CompositeView = require('views/CompositeView'),
         env = require('env'),
-        template = require('hbs!templates/OutageMap');
+        template = require('hbs!templates/OutageMap'),
+        regionHelpers = require('region-helpers');
 
     var OutageMapView = CompositeView.extend({
         initialize: function (options) {
@@ -14,6 +15,7 @@
             options || (options = {});
             this.dispatcher = options.dispatcher || this;
             this.region = options.region || 'allofaep';
+            this.listenTo(this.model, 'sync', this.updateViewFromModel);
         },
 
         resources: function (culture) {
@@ -27,17 +29,33 @@
             var renderModel = _.extend({}, this.resources(), this.model);
             this.$el.html(template(renderModel));
 
-            require(['svg!maps/' + this.region], function (map) {
-                var svgElement = document.getElementById('svg-container');
-                if (svgElement) {
-                    svgElement.innerHTML = map;
-                    //env.attachEvents(this.model.get('operatingCompanies).operatingCompanies[3].states[0].incidents);
-                }
-            });
+
             return this;
         },
         showOutageReportView: function () {
             console.trace('OutageMapView.showOutageReportView()');
+        },
+        updateViewFromModel: function () {
+            var currentContext = this;
+            require(['svg!maps/' + this.region], function (map) {
+                var svgElement = document.getElementById('svg-container');
+                if (svgElement) {
+                    svgElement.innerHTML = map;
+                    var operatingCompany = regionHelpers.getOperatingCompanyById(env.getParameterByName('region'));
+                    var operatingCompanyModel = currentContext.model.getOperatingCompany(operatingCompany.identifier);
+                    if (operatingCompanyModel && operatingCompanyModel.states && operatingCompanyModel.states.length > 0) {
+                        _.each(operatingCompanyModel.states, function (state) {
+                            if (state.customersAffected > 0) {
+                                if (state.incidents && state.incidents.length > 0) {
+                                    env.attachEvents(state.incidents);
+                                }
+                                var stateCopy = _.clone(state);
+                                renderModel.states.push(stateCopy);
+                            }
+                        });
+                    }
+                }
+            });
         }
     });
 
