dc473b1109247dde2c5d5b6c422f8855f647d9de
Michael Baltic <michaelbaltic@Michaels-MacBook-Pro.local>
2014-06-19 09:16:18 -0400

    style updates

--- app/secure/js/main.js
+++ app/secure/js/main.js
@@ -5,7 +5,7 @@ require.config({
         },
         'env': {
             'apiUrl': '',
-            'appFolder': '/secure', /*'appFolder': '/~michaelbaltic/outagemap/app/secure',*/
+            'appFolder': '/~michaelbaltic/outagemap/app/secure',
             'refreshInterval': 30000, /*30 seconds*/
             'dataExpiration': 3600000,/*1 hour*/
             'disclaimer': 'NOTE&#58; This system uses outage information reported by our customers to create a computer projection of the total number of customers affected by an outage event. During fast&#45;changing situations &#8211; such as a major storm or similar event &#8211; the accuracy of this estimate can be affected by a large number of unusual situations that must be analyzed.',
