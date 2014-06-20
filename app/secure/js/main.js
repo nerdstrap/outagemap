71f828b5e0cb9f92bb05b794b1e671f1e9440e55
nerdstrap <geekstrap@live.com>
2014-06-20 09:33:10 -0400

    css and web.config merge

--- app/secure/js/main.js
+++ app/secure/js/main.js
@@ -5,7 +5,7 @@ require.config({
         },
         'env': {
             'apiUrl': '/app',
-            'appFolder': '/app/secure', /*'appFolder': '/~michaelbaltic/outagemap/app/secure'*/
+            'appFolder': '/secure', /*'appFolder': '/~michaelbaltic/outagemap/app/secure'*/
             'refreshInterval': 30000, /*30 seconds*/
             'dataExpiration': 3600000,/*1 hour*/
             'incidentTotalThreshold': 100,
