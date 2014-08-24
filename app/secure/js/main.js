ff3a5b9232cb15257b0de300ee6188c40f274c12
Michael Baltic <michaelbaltic@Michaels-MacBook-Pro.local>
2014-08-24 19:16:53 -0400

    added swepco map

--- app/secure/js/main.js
+++ app/secure/js/main.js
@@ -6,7 +6,7 @@ require.config({
         },
         'env': {
             'apiUrl': '',
-            'appFolder': '',
+            'appFolder': '/~michaelbaltic/outagemap/app/secure',
             'refreshInterval': 30000, /*30 seconds*/
             'dataExpiration': 3600000,/*1 hour*/
             'incidentTotalThreshold': 100
